5c830278875aa0ba5ff4c71b7dad16c81ba042cb
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-12-21 11:37:37 -0600

    getting firebase working

---
+++ src/javascripts/helpers/data/planetsData.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys';
+
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getAllPlanets = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/planets.json`)
+    .then((response) => {
+      const demPlanets = response.data;
+      const planets = [];
+      Object.keys(demPlanets).forEach((pId) => {
+        demPlanets[pId].id = pId;
+        planets.push(demPlanets[pId]);
+      });
+      resolve(planets);
+    })
+    .catch((error) => reject(error));
+});
+
+export default { getAllPlanets };
